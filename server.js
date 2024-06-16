f52362b519eb3bd73266b9129376e24d4850318d
Dayana Keo <keo.dayana@hotmail.fr>
2024-06-16 22:36:40 +0200

    Ajout de la route rôle

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config()
+
+//DECLARATION ROUTE
 const userRouter = require('./route/user');
-const sequelize = require('./models/User');
+const roleRouter = require('./route/role');
 
 
 const app = express();
@@ -12,13 +15,15 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//ROUTE USER
 app.use('/api/user', userRouter);
+app.use('/api/role', roleRouter);
 
-// app.listen(port, () => {
-//   console.log(`Server is running on http://localhost:${port}`);
-// });
-
-sequelize.sync({ force: true }).then(() => {
+app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
-  // Votre serveur commence ici
 });
+
+// sql.sync({ force: true }).then(() => {
+//   console.log(`Server is running on http://localhost:${port}`);
+//   // Votre serveur commence ici
+// });
