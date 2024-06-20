30ef93b78afc87ca373a3f0ff555f91da7868a14
Dayana Keo <keo.dayana@hotmail.fr>
2024-06-20 10:22:22 +0200

    Refactorisation du modèle User pour utiliser des promesses au lieu des callback

--- model/user.js
+++ model/user.js
@@ -15,34 +15,81 @@ const User = function(user) {
   this.two_factor_enabled = user.two_factor_enabled || false;
 };
 
-User.create = async (newUser, result) => {
-  try {
+User.create = (newUser) => {
+  return new Promise((resolve, reject) => {
     sql.query("INSERT INTO user SET ?", newUser, (err, res) => {
       if (err) {
         console.log("error: ", err);
-        result(err, null);
+        reject(err);
         return;
       }
       console.log("Création de l'utilisateur: ", { id: res.insertId, ...newUser });
-      result(null, { id: res.insertId, ...newUser });
+      resolve({ id: res.insertId, ...newUser });
     });
-  } catch (error) {
-    result(error, null);
-  }
-};
-
-User.saveUser = (user, result) => {
-  const query = "UPDATE user SET two_factor_secret = ?, two_factor_enabled = ? WHERE id = ?";
-  sql.query(query, [user.two_factor_secret, user.two_factor_enabled, user.id], (err, res) => {
-      if (err) {
-          console.log("Erreur lors de la sauvegarde de l'utilisateur :", err);
-          result(err, null);
-          return;
-      }
-      result(null, res);
   });
 };
 
+User.saveUser = (user) => {
+  return new Promise((resolve, reject) => {
+    const query = 'UPDATE user SET email_activate = ?, two_factor_enabled = ?, two_factor_secret = ? WHERE id = ?';
+    sql.query(query, [user.email_activate, user.two_factor_enabled, user.two_factor_secret, user.id], (err, res) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res);
+      }
+    });
+  });
+}
+
+User.findById = (userId) => {
+  return new Promise((resolve, reject) => {
+    sql.query("SELECT * FROM user WHERE id = ?", [userId], (err, res) => {
+      if (err) {
+        reject(err);
+      } else if (res.length) {
+        resolve(res[0]);
+      } else {
+        resolve(null);
+      }
+    });
+  });
+};
+
+User.findByEmail = (email) => {
+  return new Promise((resolve, reject) => {
+    sql.query("SELECT * FROM user WHERE email = ?", [email], (err, res) => {
+      if (err) {
+        reject(err);
+      } else if (res.length) {
+        resolve(res[0]);
+      } else {
+        resolve(null);
+      }
+    });
+  });
+};
+                
+                
+User.updateEmailActivate = (userId, callback) => {
+  sql.query(
+    'UPDATE user SET email_activate = true WHERE id = ?',
+    [userId],
+    (err, res) => {
+      if (err) {
+        console.error('Error updating email activation:', err);
+        callback(err, null);
+        return;
+        }
+        console.log('Email activation updated successfully');
+        const activationLink = `http://localhost:4000/activate-2fa?userId=${userId}`;
+        
+        // Envoyer le callback avec le lien d'activation 2FA
+        callback(null, { message: 'Email activation updated successfully', activationLink });
+      }
+  );
+}
+                        
 User.findAll = (result) => {
   sql.query('SELECT * FROM user', (err, res) => {
     if (err) {
@@ -54,74 +101,6 @@ User.findAll = (result) => {
     result(null, res);
   });
 };
-
-User.findById = (userId, result) => {
-  sql.query("SELECT * FROM user WHERE id = ?", [userId], (err, res) => {
-      if (err) {
-          console.log("error: ", err);
-          result(err, null);
-          return;
-      }
-      if (res.length) {
-          result(null, res[0]);
-          return;
-      }
-      result({ message: "Utilisateur non trouvé" }, null);
-  });
-};
-User.findByEmail = (email, result) => {
-  sql.query("SELECT * FROM user WHERE email = ?", [email], (err, res) => {
-    if (err) {
-      console.log("error: ", err);
-      result(err, null);
-      return;
-    }
-    if (res.length) {
-      result(null, res[0]);
-      return;
-    }
-    result(null, null);
-  });
-};
-
-
-User.authenticate = async (email, password) => {
-  return new Promise((resolve, reject) => {
-    sql.query("SELECT * FROM user WHERE email = ?", [email], async (err, res) => {
-      if (err) {
-        return reject(err);
-      }
-      if (res.length) {
-        const user = res[0];
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (isMatch) {
-          return resolve(user);
-        }
-      }
-      return reject('Authentication failed');
-    });
-  });
-};
-
-User.updateEmailActivate = (userId, callback) => {
-  sql.query(
-    'UPDATE user SET email_activate = true WHERE id = ?',
-    [userId],
-    (err, res) => {
-      if (err) {
-        console.error('Error updating email activation:', err);
-        callback(err, null);
-        return;
-      }
-      console.log('Email activation updated successfully');
-      const activationLink = `http://localhost:4000/activate-2fa?userId=${userId}`;
-
-      // Envoyer le callback avec le lien d'activation 2FA
-      callback(null, { message: 'Email activation updated successfully', activationLink });
-    }
-  );
-}
-
 User.updatePassword = (userId, newPassword, result) => {
   bcrypt.hash(newPassword, 10, (err, hashedPassword) => {
     if (err) {
@@ -242,44 +221,44 @@ User.clearResetToken = (userId, result) => {
   );
 };
 
-User.verifyTwoFactorToken = (userId, token, callback) => {
-  sql.query('SELECT * FROM user WHERE id = ?', [userId], async (err, res) => {
-    if (err) {
-      console.error('Erreur lors de la recherche de l\'utilisateur :', err);
-      return callback(err, null);
-    }
+// User.verifyTwoFactorToken = (userId, token, callback) => {
+//   sql.query('SELECT * FROM user WHERE id = ?', [userId], async (err, res) => {
+//     if (err) {
+//       console.error('Erreur lors de la recherche de l\'utilisateur :', err);
+//       return callback(err, null);
+//     }
 
-    if (res.length === 0) {
-      return callback({ message: 'Utilisateur non trouvé' }, null);
-    }
+//     if (res.length === 0) {
+//       return callback({ message: 'Utilisateur non trouvé' }, null);
+//     }
 
-    const user = res[0];
+//     const user = res[0];
 
-    if (!user.two_factor_secret) {
-      return callback({ message: 'Secret 2FA non généré pour l\'utilisateur' }, null);
-    }
+//     if (!user.two_factor_secret) {
+//       return callback({ message: 'Secret 2FA non généré pour l\'utilisateur' }, null);
+//     }
 
-    const verified = speakeasy.totp.verify({
-      secret: user.two_factor_secret,
-      encoding: 'base32',
-      token: token,
-      digits: 5,
-      step: 3600
-    });
+//     const verified = speakeasy.totp.verify({
+//       secret: user.two_factor_secret,
+//       encoding: 'base32',
+//       token: token,
+//       digits: 5,
+//       step: 3600
+//     });
 
-    if (verified) {
-      // Mettre à jour l'état 2FA de l'utilisateur à true
-      sql.query('UPDATE user SET two_factor_enabled = ? WHERE id = ?', [true, userId], (err, result) => {
-        if (err) {
-          console.error('Erreur lors de la mise à jour du statut 2FA :', err);
-          return callback(err, null);
-        }
-        callback(null, { message: 'Code vérifié avec succès' });
-      });
-    } else {
-      callback({ message: 'Code invalide' }, null);
-    }
-  });
-};
+//     if (verified) {
+//       // Mettre à jour l'état 2FA de l'utilisateur à true
+//       sql.query('UPDATE user SET two_factor_enabled = ? WHERE id = ?', [true, userId], (err, result) => {
+//         if (err) {
+//           console.error('Erreur lors de la mise à jour du statut 2FA :', err);
+//           return callback(err, null);
+//         }
+//         callback(null, { message: 'Code vérifié avec succès' });
+//       });
+//     } else {
+//       callback({ message: 'Code invalide' }, null);
+//     }
+//   });
+// };
 
 module.exports = User;
