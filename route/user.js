0cb0a1f05a35e34f34723852e7278f8640791cc9
Dayana Keo <keo.dayana@hotmail.fr>
2024-06-20 10:15:47 +0200

    Modification des routes

--- route/user.js
+++ route/user.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controller/user/user');
+const userController = require('../controller/user/userController');
+const authController = require('../controller/user/authController');
 
 
 router.post('/register', userController.create);
 router.get('/getUser', userController.findAll);
-router.post('/auth', userController.auth);
+// router.post('/login', userController.login);
 router.post('/send-verification-email', userController.sendVerificationEmail);
 router.get('/verify/:token', userController.verifyEmail);
 router.post('/forgot-password', userController.forgetPassword);
 router.get('/reset-password/:token', userController.resetPassword);
 router.post('/update-password', userController.updatePassword);
+
+router.post('/login', authController.login);
+router.post('/verify-2fa-code', authController.verifyTokenWithUserId);
+router.get('/verify-2fa', authController.showVerify2faPage);
+router.get('/activate-2fa', authController.sendActivate2fa);
+
+
+
 
 module.exports = router;
