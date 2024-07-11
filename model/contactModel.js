623368203370776157533d566622e448bc700ad1
Dayana Keo <keo.dayana@hotmail.fr>
2024-07-12 00:24:32 +0200

    Methode de gestion des contact d'urgence

--- model/contactModel.js
+++ model/contactModel.js
@@ -5,7 +5,8 @@ const Contact = function(contact) {
     this.nom = contact.nom;
     this.tel = contact.tel;
     this.lien_affilie = contact.lien_affilie;
-    this.tuteur_id = contact.tuteur_id
+    this.user_id = contact.user_id;
+    this.child_id = contact.child_id
 };
 
 Contact.create = (newContact) => {
@@ -20,6 +21,72 @@ Contact.create = (newContact) => {
         resolve({ id: res.insertId, ...newContact });
       });
     });
+};
+
+Contact.getContactByUserId = (userId) => {
+  return new Promise((resolve, reject) => {
+    const query = "SELECT * FROM contact_urg WHERE user_id = ?";
+    sql.query(query, [userId], (err, results) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(results);
+    });
+  });
+};
+
+Contact.updateById = (id, contact) => {
+  return new Promise((resolve, reject) => {
+    sql.query(
+      "UPDATE contact_urg SET ? WHERE id = ?",
+      [contact, id],
+      (err, result) => {
+        if (err) {
+          console.error("Erreur lors de la mise à jour du contact:", err);
+          reject(err);
+          return;
+        }
+
+        if (result.affectedRows === 0) {
+          reject({ kind: "not_found" });
+          return;
+        }
+
+        console.log("Contact mis à jour:", { id, ...contact });
+        resolve({ id, ...contact });
+      }
+    );
+  });
+};
+
+
+Contact.deleteById = (id) => {
+  return new Promise((resolve, reject) => {
+    // Commencer par supprimer les enregistrements liés dans `contact_urg`
+    sql.query("DELETE FROM child WHERE contact_id = ?", id, (err, result) => {
+      if (err) {
+        console.error("Erreur lors de la suppression des contacts d'urgence:", err);
+        reject(err);
+        return;
+      }
+
+      // Supprimer le contact après avoir supprimé les enregistrements liés dans `contact_urg`
+      sql.query("DELETE FROM contact_urg WHERE id = ?", id, (err, result) => {
+        if (err) {
+          console.error("Erreur lors de la suppression du contact:", err);
+          reject(err);
+          return;
+        }
+
+        if (result.affectedRows === 0) {
+          reject({ kind: "not_found" });
+          return;
+        }
+
+        resolve(result);
+      });
+    });
+  });
 };
 
 module.exports = Contact;
--- route/contactRoute.js
+++ route/contactRoute.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const contactController = require('../controller/contact_urg/contactController')
+const contactController = require('../controller/contact_urg/contactController');
+const authenticateJWT = require('../middleware/authMiddleware');
 
-router.post('/ajouter-un-contact', contactController.create);
-
+router.post('/ajouter-un-contact/:userId', authenticateJWT, contactController.create);
+router.get('/:userId/contacts', contactController.getContactByUserId);
+router.put('/editer-contact/:id', authenticateJWT, contactController.updateContactById)
+router.delete('/supprimer/:id', authenticateJWT, contactController.deleteContactById);
 module.exports = router;
