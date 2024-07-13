108976738b6b450c0ca04c309b4a68f77fb37e33
Dayana Keo <keo.dayana@hotmail.fr>
2024-07-13 19:25:17 +0200

    déclaration de route

--- route/tuteurRoute.js
+++ route/tuteurRoute.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const tuteurController = require('../controller/tuteur/tuteurController');
+const authenticateJWT = require('../middleware/authMiddleware');
 
 router.post('/ajout-tuteur', tuteurController.create);
+router.get('/get-tuteur-by-user/:id', tuteurController.findTuteurByUserId);
+router.get('/get-tuteur-id/:id', tuteurController.findById);
+router.get('/liste-enfant/:id', tuteurController.getChildrenByTuteurUserId);
+router.put('/editer-tuteur', authenticateJWT, tuteurController.updateTuteur);
 
 module.exports = router;
